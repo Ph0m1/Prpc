6e8d8531b5416a9504cbe6b20e2ee3cf9b407e41
Ph0m1 <ph0m.xupt@gmail.com>
2025-03-01 21:53:19 +0800

    add logger.hpp

---
+++ src/logger.hpp
@@ -0,0 +1,34 @@
+#ifndef P_LOG_HPP
+#define P_LOG_HPP
+#include <glog/logging.h>
+#include <string>
+
+class PLogger{
+    public:
+    explicit PLogger(const char* argv0){
+        google::InitGoogleLogging(argv0);
+        FLAGS_colorlogtostderr = true;
+        FLAGS_logtostderr = true;
+    }
+    ~PLogger(){
+        google::ShutdownGoogleLogging();
+    }
+    static void Info(const std::string& message){
+        LOG(INFO) << message;
+    }
+    static void Warning(const std::string& message){
+        LOG(WARNING)<<message;
+      }
+    static void ERROR(const std::string& message){
+        LOG(ERROR)<<message;
+    }
+    static void Fatal(const std::string& message) {
+        LOG(FATAL) << message;
+    }
+
+    private:
+    PLogger(const PLogger&)=delete;
+    PLogger& operator=(const PLogger&)=delete;
+};
+
+#endif
