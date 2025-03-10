3526af34bf105550bd5953b23edfb991be31be6d
Ph0m1 <ph0m.xupt@gmail.com>
2025-03-10 14:14:43 +0800

    optimize logger format

--- src/logger.hpp
+++ src/logger.hpp
@@ -1,34 +1,25 @@
 #ifndef P_LOG_HPP
 #define P_LOG_HPP
 #include <glog/logging.h>
+
 #include <string>
 
-class PLogger{
-    public:
-    explicit PLogger(const char* argv0){
-        google::InitGoogleLogging(argv0);
-        FLAGS_colorlogtostderr = true;
-        FLAGS_logtostderr = true;
-    }
-    ~PLogger(){
-        google::ShutdownGoogleLogging();
-    }
-    static void Info(const std::string& message){
-        LOG(INFO) << message;
-    }
-    static void Warning(const std::string& message){
-        LOG(WARNING)<<message;
-      }
-    static void ERROR(const std::string& message){
-        LOG(ERROR)<<message;
-    }
-    static void Fatal(const std::string& message) {
-        LOG(FATAL) << message;
-    }
+class PLogger {
+ public:
+  explicit PLogger(const char* argv0) {
+    google::InitGoogleLogging(argv0);
+    FLAGS_colorlogtostderr = true;
+    FLAGS_logtostderr = true;
+  }
+  ~PLogger() { google::ShutdownGoogleLogging(); }
+  static void Info(const std::string& message) { LOG(INFO) << message; }
+  static void Warning(const std::string& message) { LOG(WARNING) << message; }
+  static void ERROR(const std::string& message) { LOG(ERROR) << message; }
+  static void Fatal(const std::string& message) { LOG(FATAL) << message; }
 
-    private:
-    PLogger(const PLogger&)=delete;
-    PLogger& operator=(const PLogger&)=delete;
+ private:
+  PLogger(const PLogger&) = delete;
+  PLogger& operator=(const PLogger&) = delete;
 };
 
 #endif
