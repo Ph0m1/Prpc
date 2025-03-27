4c6c3adb9673b0646ca5fcaf2448cbbb0d0a5028
Ph0m1 <ph0m.xupt@gmail.com>
2025-03-27 23:01:13 +0800

    Add a demo to try running RPC

---
+++ sample/callee/server.cc
@@ -0,0 +1,46 @@
+#include "../user.pb.h"
+#include "application.hpp"
+#include "provider.hpp"
+
+class UserService : public Puser::UserServiceRpc {
+ public:
+  // simulate login logic
+  bool Login(std::string name, std::string pwd) {
+    std::cout << "doing local service: Login" << std::endl;
+    std::cout << "name: " << name << " pwd: " << pwd << std::endl;
+    return true;
+  }
+
+  void Login(::google::protobuf::RpcController* controller,
+             const ::Puser::LoginRequest* request,
+             ::Puser::LoginResponse* response,
+             ::google::protobuf::Closure* done) {
+    // get username an passwd
+    std::string name = request->name();
+    std::string pwd = request->pwd();
+
+    // login
+    bool login_result = Login(name, pwd);
+
+    // write result to response
+    Puser::ResultCode* code = response->mutable_result();
+    code->set_errcode(0);                 // err = 0, success
+    code->set_errmsg("");                 // errinfo = null
+    response->set_success(login_result);  // set login
+
+    done->Run();
+  }
+};
+
+int main(int argc, char** argv) {
+  // init
+  Papplication::Init(argc, argv);
+  Pprovider provider;
+
+  // notify UserService to RPC node
+  provider.NotifyService(new UserService());
+
+  // run RPC node, waitting for RPC
+  provider.Run();
+  return 0;
+}
